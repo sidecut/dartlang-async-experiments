46096757d1167ceb2ef9381335b993e2cdb1bb14
Jim Raden <james.raden@gmail.com>
2019-02-20 20:40:56 -0500

    Create project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:signals_test/signals_test.dart' as signals_test;
+
+main(List<String> arguments) {
+  print('Hello world: ${signals_test.calculate()}!');
+}
---
+++ test/signals_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:signals_test/signals_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
