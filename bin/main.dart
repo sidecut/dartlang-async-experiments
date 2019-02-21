4299b749d72de7543c165ca605ff2e74030f3c1b
Jim Raden <james.raden@gmail.com>
2019-02-20 20:53:29 -0500

    Proof of concept of a simple future

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,25 @@
-import 'package:signals_test/signals_test.dart' as signals_test;
+import 'dart:async';
 
-main(List<String> arguments) {
-  print('Hello world: ${signals_test.calculate()}!');
+main(List<String> arguments) async {
+  // Run a task that will take a while
+  _longTaskStatus = longTask();
+
+  print("${DateTime.now()}\tStarting");
+  for (var i = 0; i < 10; i++) {
+    // Run a task that waits for the first task
+    shortTask();
+    await Future.delayed(Duration(milliseconds: 250));
+  }
+}
+
+Future _longTaskStatus;
+
+Future longTask() {
+  return Future.delayed(Duration(milliseconds: 1500));
+}
+
+void shortTask() {
+  _longTaskStatus.then((value) {
+    print("${DateTime.now()}\tlong Task is complete");
+  });
 }
