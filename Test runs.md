217f7b2840e33b3ee2651db854117790672c2661
Jim Raden <james.raden@gmail.com>
2019-02-22 18:13:13 -0500

    Feb 21 08:20: Prove out simple awaitable signal

---
+++ Test runs.md
@@ -0,0 +1,21 @@
+# Test runs
+
+## Using `first`
+
+```
+2019-02-21 08:19:27.507939	Starting
+2019-02-21 08:19:29.009638	Short task 0 is complete
+2019-02-21 08:19:29.009638	Short task 1 is complete
+2019-02-21 08:19:29.009638	Short task 2 is complete
+2019-02-21 08:19:29.009638	Short task 3 is complete
+2019-02-21 08:19:29.009638	Short task 4 is complete
+2019-02-21 08:19:29.010601	Short task 5 is complete
+2019-02-21 08:19:29.043601	Short task 6 is complete
+2019-02-21 08:19:29.294355	Short task 7 is complete
+2019-02-21 08:19:29.546503	Short task 8 is complete
+2019-02-21 08:19:29.797584	Short task 9 is complete
+Exited
+```
+
+## Using `last`
+Doesn't work.  Never returns because it's waiting for the last item, which never comes, right?
--- bin/main.dart
+++ bin/main.dart
@@ -1,25 +1,31 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+
+BehaviorSubject<bool> _observable = BehaviorSubject<bool>();
+Future<bool> get _ready => _observable.first;
 
 main(List<String> arguments) async {
   // Run a task that will take a while
-  _longTaskStatus = longTask();
+  longTask().then((value) {
+    _observable.add(value);
+  });
 
   print("${DateTime.now()}\tStarting");
   for (var i = 0; i < 10; i++) {
     // Run a task that waits for the first task
-    shortTask();
+    shortTask(i);
     await Future.delayed(Duration(milliseconds: 250));
   }
 }
 
-Future _longTaskStatus;
+//Future _longTaskStatus;
 
 Future longTask() {
   return Future.delayed(Duration(milliseconds: 1500));
 }
 
-void shortTask() {
-  _longTaskStatus.then((value) {
-    print("${DateTime.now()}\tlong Task is complete");
+void shortTask(int i) {
+  _ready.then((value) {
+    print("${DateTime.now()}\tShort task $i is complete");
   });
 }
